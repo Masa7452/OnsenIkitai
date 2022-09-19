df742601090ce4b26a05c30a07da1a9c84ff2c74
Masakazu7452 <saintetienne218@gmail.com>
2022-09-19 21:01:12 +0900

    検索モーダル開くところまで

---
+++ src/components/modals/MapModal.js
@@ -0,0 +1,45 @@
+import { css } from "@emotion/react";
+import React, { memo, useState } from "react";
+import { Modal } from "semantic-ui-react";
+
+const Layout = ( { params } )=> 
+{
+    return (
+        <Modal
+            open={params.isOpen}
+            onClose={() => params.onClose()}
+            // onOpen={() => setOpen(true)}
+            // trigger={<Button>Scrolling Content Modal</Button>}
+            >
+            <Modal.Header>Map Modal</Modal.Header>
+        </Modal>
+    );
+}
+
+const Style = ( params ) => css`
+`;
+
+const MapModal = memo(( props ) => 
+{
+    const [ isOpen, setIsOpen ] = useState( true );
+
+    const onClose = () => 
+    {
+        setIsOpen( false );
+        props.setSearchMode( `none` );
+    }
+
+    const styleParams=
+    {
+    }
+
+    const params = 
+    {
+        style   : Style( styleParams ),
+        isOpen  : isOpen,
+        onClose : onClose,
+    }
+    return <Layout params={params} />
+});
+
+export default MapModal;
